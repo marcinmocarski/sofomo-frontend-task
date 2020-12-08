fa0a01bd8de9205430c6f8940eb5af49a6c3d579
unknown <mocarski.marcin.93@gmail.com>
2020-12-09 00:27:39 +0100

    Changing back to https request

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,2 +1,2 @@
 export const IP_ADDRESS_API_URL = "https://api.ipify.org/?format=json";
-export const IP_ADDRESS_GEOLOCATION_API_URL = "http://api.ipstack.com";
+export const IP_ADDRESS_GEOLOCATION_API_URL = "https://api.ipstack.com";
